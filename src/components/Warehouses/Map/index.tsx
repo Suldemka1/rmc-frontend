890e6b47d4ccdbda0e0e3ee15bc22b9415824d81
Suldemka1 <suldemka1@gmail.com>
2022-11-07 22:15:56 +0700

    commit

--- src/components/Warehouses/Map/index.tsx
+++ src/components/Warehouses/Map/index.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {GeoJSON, MapContainer, Marker, Popup, TileLayer, Tooltip} from "react-leaflet";
+import polygons from '../../../data.json'
+import {Link} from "react-router-dom";
 
 const Map = () => {
+    const renderRepublicBoundaries = polygons.map(item => {
 
+        return (
+            <Link to="/"><GeoJSON
+                key={item.id}
+                // @ts-ignore
+                data={item}
+                style={() => ({
+                    color: '#4a83ec',
+                    weight: 1,
+                    fillColor: "#1a1d62",
+                    fillOpacity: 1
+                })}
+                className="hover:fill-white"
+            >
+                <Tooltip sticky>{item.properties.description}</Tooltip>
+            </GeoJSON>
+            </Link>
+        )
+
+    })
     return (
-        <div className="min-h-[300px] max-h-[600px] min-w-[300px] max-w-full w-full h-full border-2 border-black rounded">
-            <MapContainer center={[51.7, 94.4]} zoom={7} scrollWheelZoom={false} attributionControl={false} className="w-full h-full">
+        <div
+            className=" max-h-[600px] max-w-full w-full h-full border-2 border-black rounded">
+            <MapContainer center={[51.8, 94.15]}
+                          zoom={7}
+                          zoomControl={false}
+                          scrollWheelZoom={false}
+                          doubleClickZoom={false}
+                          attributionControl={false}
+                          dragging={false}
+
+                          className="w-full h-full">
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    opacity={0}
                 />
 
-                <Marker position={[51.7, 94.4]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {renderRepublicBoundaries}
 
             </MapContainer>
         </div>
